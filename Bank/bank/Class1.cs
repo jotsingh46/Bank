ac618df9c11d4bb5abc444cc8c9f91acf41e38c0
visualstudio <visualstudio@230-PC29>
2019-03-14 15:10:12 -0400

    assignment3_c0730244

--- Bank/bank/Class1.cs
+++ Bank/bank/Class1.cs
@@ -57,7 +57,7 @@ namespace BankAccountNS
             {
                 throw new ArgumentOutOfRangeException("amount");
             }
-            m_balance += amount;
+            m_balance -= amount;
         }
         private void FreezeAccount()
         {
